d62aff896a0755b441635c764126e3467fe0e107
c <c@mba-2.local>
2017-02-12 13:21:29 -0500

    cleans up active admin events table view

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -16,4 +16,21 @@ ActiveAdmin.register Event do
   permit_params :event_type,:moc_id,:date,:time,:time_zone,
     :location,:street_address,:state,:zip,:notes
 
+  index do
+    column "Member" do |e|
+      e.moc.name
+    end
+    column :event_type
+    column :date
+    column :time
+    column :time_zone
+    column :location
+    column :street_address
+    column :state
+    column "District" do |e|
+      e.moc.district
+    end
+    column :zip
+    column :notes
+  end
 end

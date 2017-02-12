497a634201493fa9db08018372865cfca2d5a725
c <c@mba-2.local>
2017-02-12 15:04:51 -0500

    sets the route constraings for the api

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'welcome#index'
+  namespace :api, defaults: { format: :json },
+                                  constraints: { subdomain: 'api' }, path: '/'  do
+  end
 end
